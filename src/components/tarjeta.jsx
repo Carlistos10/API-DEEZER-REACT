56ad505e62630b8ed9295eb9b3493eb42b0c2626
Carlistos10 <117452354+Carlistos10@users.noreply.github.com>
2024-03-07 22:22:11 +0100

    Update tarjeta.jsx

--- src/components/tarjeta.jsx
+++ src/components/tarjeta.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from 'react';
+import './tarjeta.css'
 
-export const Tarjeta = () => {
+export const Tarjeta = ({ data }) => {
+    const {
+        title,
+        album,
+        artist,
+
+
+    } = data;
+
     return (
-        <div>tarjeta</div>
-    )
-}
+        <div className="container">
+            <div className="card">
+                <div className="imgBx">
+                    <img src={`${album.cover_medium}`} alt='' />
+                </div>
+                <div className="contentBx">
+                    <h2>{title}</h2>
+                    <div className="size">
+                        <h3>Artista :</h3>
+                        <span>{artist.name}</span>
+                    </div>
+                    <div className="color">
+                        <h3>Album :</h3>
+                        <span>{album.title}</span>
+                    </div>
+                    <a className="esp" href="#">Mas info</a>
+                    {/*Deberia llevar a informacion mas detallada de la cancion*/}
+                </div>
+            </div>
+        </div>
+    );
+};
