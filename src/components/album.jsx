d4c16f49bf0e3b20dc73fdcf5503f1d8eb8d0579
Carlistos10 <117452354+Carlistos10@users.noreply.github.com>
2024-03-12 05:01:31 +0100

    FInal album.jsx

--- src/components/album.jsx
+++ src/components/album.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { useParams, Link } from 'react-router-dom';
 
 export const Album = () => {
+    const { album } = useParams(); // Obtén los parámetros de la URL
+
+
     return (
-        <div>API ALBUM</div>
-    )
+        <div>
+            <h1>Detalles del álbum: {album}</h1>
+            <p>Este album pertenece a un gran artista, el cual dedica mucho tiempo e invierte una gran cantidad de dinero para lograr la maxima calidad en sus proyectos </p> {/* Muestra el nombre del artista */}
+            <Link to={`/`}>
+                <span>Volver</span>
+            </Link>
+        </div>
+    );
 }
