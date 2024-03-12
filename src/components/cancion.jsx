32d8297642ca05847e4c078ec71b7314cf05dd22
Carlistos10 <117452354+Carlistos10@users.noreply.github.com>
2024-03-12 05:00:12 +0100

    Final cancion.jsx

--- src/components/cancion.jsx
+++ src/components/cancion.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { useParams, Link } from 'react-router-dom';
 
-export const Cancion = () => {
-    return (
-        <div>cancion</div>
-    )
-}
+const Cancion = () => {
+  const { artist, album, cancion } = useParams(); // Obtén los parámetros de la URL
+
+  return (
+    <div>
+      <h1>Detalles de la Canción: {cancion}</h1>
+      <p>Artista: {artist}</p>
+      <p>Álbum: {album}</p>
+      <Link to={`/`}>
+        <span>Volver</span>
+      </Link>
+    </div>
+  );
+};
+
+export default Cancion;
