2a331cdfd17b09c396e78e300bad99ae9e3a7adf
Carlistos10 <117452354+Carlistos10@users.noreply.github.com>
2024-03-12 05:01:11 +0100

    Final artista.jsx

--- src/components/artista.jsx
+++ src/components/artista.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { useParams, Link } from 'react-router-dom';
 
 export const Artista = () => {
+    const { artist } = useParams(); // Obtén los parámetros de la URL
+
+
     return (
-        <div>Artista</div>
-    )
-}
+        <div>
+            <h1>Detalles del artista: {artist}</h1>
+            <p>Este artista tiene una gran variedad de albumes, y ha colaborado en una gran variedad de proyectos</p>
+            <Link to={`/`}>
+                <span>Volver</span>
+            </Link>
+        </div>
+    );
+};
